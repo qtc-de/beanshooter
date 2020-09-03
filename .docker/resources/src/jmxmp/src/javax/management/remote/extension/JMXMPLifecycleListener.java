c621ca39b91770ecc560a7576b2e6b6f2aab3011
TNeitzel <qtc_de@outlook.com>
2020-09-03 07:51:41 +0200

    Update docker image
    The docker image now support TLS protected JMX and uses a different
    JMXMP listener.

---
+++ .docker/resources/src/jmxmp/src/javax/management/remote/extension/JMXMPLifecycleListener.java
@@ -0,0 +1,56 @@
+/* Credits: https://github.com/felixoldenburg/jmxmp-lifecycle-listener */
+package javax.management.remote.extension;
+
+import java.lang.management.ManagementFactory;
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXConnectorServerFactory;
+import javax.management.remote.JMXServiceURL;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleListener;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
+public class JMXMPLifecycleListener implements LifecycleListener
+{
+    protected int port = 5555;
+    protected JMXConnectorServer cs;
+    private static final Log log = LogFactory.getLog(JMXMPLifecycleListener.class);
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(final int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void lifecycleEvent(final LifecycleEvent event)
+    {
+        try {
+
+            if (Lifecycle.START_EVENT == event.getType()) {
+                log.debug("Starting JMXMP");
+
+                cs = JMXConnectorServerFactory.newJMXConnectorServer(
+                    new JMXServiceURL("jmxmp", "0.0.0.0", port),
+                    null,
+                    ManagementFactory.getPlatformMBeanServer()
+                );
+                cs.start();
+
+                log.info("Started JMXMP on port " + port);
+            }
+
+            else if (Lifecycle.STOP_EVENT == event.getType()) {
+                log.debug("Stopp JMXMP");
+                cs.stop();
+                log.info("Stopped JMXMP");
+            }
+
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
