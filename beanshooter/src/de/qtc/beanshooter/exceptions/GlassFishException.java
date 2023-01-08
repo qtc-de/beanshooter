3ae741257829966a7dc8b877c160786d4360511e
Tobias Neitzel <secure@tneitzel.eu>
2023-01-08 16:29:48 +0100

    Add custom printStackTrace for GlassFishException
    The GlassFishException has now a custom printStackTrace function that
    outputs an additional indicator that the stacktrace is misleading.

--- beanshooter/src/de/qtc/beanshooter/exceptions/GlassFishException.java
+++ beanshooter/src/de/qtc/beanshooter/exceptions/GlassFishException.java
@@ -1,5 +1,7 @@
 package de.qtc.beanshooter.exceptions;
 
+import de.qtc.beanshooter.io.Logger;
+
 /**
  * The GlassFishException is thrown when GlassFish specific error messages
  * are observed during a JMX login attempt.
@@ -18,5 +20,18 @@ public class GlassFishException extends AuthenticationException {
     public GlassFishException(Exception e, boolean showDetails)
     {
         super(e, showDetails);
+    }
+
+    public void printStackTrace()
+    {
+        if (origException.getMessage().contains("AdminLoginModule$PrincipalCallback"))
+        {
+            Logger.lineBreak();
+            Logger.printlnMixedBlue("The following stacktrace might be misleading. See", "https://github.com/eclipse-ee4j/glassfish/issues/24223");
+            Logger.printlnMixedYellow("Summarized: The error is probably caused by", "missing or invalid", "credentials.");
+            Logger.lineBreak();
+        }
+
+        origException.printStackTrace();
     }
 }
